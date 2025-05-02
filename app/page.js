3beffd1454d9d63ebf94a6379ddd2e6b548577eb
Edersteiner <gabriel.eder@me.com>
2025-05-02 12:09:55 +0200

    Basic front page layout.

--- app/page.js
+++ app/page.js
@@ -1,15 +1,67 @@
-import Link from "next/link";
+"use client";
+import { getWeatherForecast } from "@/logic/data.js";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p>Skapa egen Main</p>
-        <Link href="/test" className="text-[blue] hover:underline">Test hämta väder</Link>
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    getWeatherForecast(59.3793, 13.5036)
+      .then((SMHIdata) => {
+        setWeatherData(SMHIdata);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (weatherData) {
+    return (
+      <div>
+        <main>
+          <DayView weatherItem={weatherData[0]}></DayView>
+        </main>
+      </div>
+    );
+  } else {
+    <div>
+      <main>
+        <h1>Loading Weather Data...</h1>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <p>Ersätt med egen footer, eller ta bort</p>
-      </footer>
+    </div>;
+  }
+}
+
+export function DayView({ weatherItem }) {
+  const date = new Date(weatherItem.time);
+  const dayString = date.toLocaleString("sv-SE", { weekday: "long" });
+  const day = dayString.charAt(0).toUpperCase() + dayString.slice(1);
+  const time = date.toLocaleString("sv-SE", {
+    hour: "numeric",
+    minute: "numeric",
+  });
+
+  return (
+    <div className="w-lvw h-3/4 flex flex-col justify-items-center justify-center items-center align-middle p-12">
+      <h2 className="text-3xl font-bold">
+        {day} | {time}
+      </h2>
+      <br></br>
+      <h2 className="text-5xl font-bold ">Karlstad</h2>
+      <br></br>
+      <Image
+        width={128}
+        height={128}
+        src={
+          "/weather-icons/weather-icon-" +
+          weatherItem.parameters.Wsymb2.value +
+          ".png"
+        }
+        alt="weather-icon"
+      />
+      <br></br>
+      <h1 className="text-5xl font-normal">
+        {weatherItem.parameters.t.value}°C
+      </h1>
     </div>
   );
 }
--- logic/data.js
+++ logic/data.js
@@ -1,40 +1,40 @@
-
-const APIentry = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/";
+const APIentry =
+  "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/";
 
 export async function getWeatherForecast(lat, lon) {
-    const smhiApiUrl = APIentry + "lon/" + lon + "/lat/" + lat + "/data.json";
-    console.log("HÃ¤mtar vÃ¤derdata...");
-    try {
-        const response = await fetch(smhiApiUrl);
-        const data = await response.json();
+  const smhiApiUrl = APIentry + "lon/" + lon + "/lat/" + lat + "/data.json";
+  console.log("Hämtar väderdata...");
+  try {
+    const response = await fetch(smhiApiUrl);
+    const data = await response.json();
 
-        // Debuggar raw respons
-        console.debug("Raw response:", data);
+    // Debuggar raw respons
+    console.debug("Raw response:", data);
 
-        // Konverterar en parameter-array till ett objekt med nycklar som parameter-namn fÃ¶r enklare anvÃ¤ndning av datan
-        function paramArrayToObject(paramArray) {
-            const paramObject = {};
-            paramArray.forEach(param => {
-                paramObject[param.name] = {
-                    value: param.values[0],
-                    unit: param.unit
-                };
-            });
-            return paramObject;
-        }
-
-        // Extrahera relevanta vÃ¤derprognoser
-        const forecasts = data.timeSeries.map(entry => ({
-            time: entry.validTime,
-            parameters: paramArrayToObject(entry.parameters)
-        }));
-
-        // Debuggar processad data
-        console.debug("Parsed response:", forecasts);
-
-        return forecasts;
-    } catch (error) {
-        console.error("Ett fel uppstod:", error);
-        throw error;
+    // Konverterar en parameter-array till ett objekt med nycklar som parameter-namn fÃ¶r enklare anvÃ¤ndning av datan
+    function paramArrayToObject(paramArray) {
+      const paramObject = {};
+      paramArray.forEach((param) => {
+        paramObject[param.name] = {
+          value: param.values[0],
+          unit: param.unit,
+        };
+      });
+      return paramObject;
     }
+
+    // Extrahera relevanta vÃ¤derprognoser
+    const forecasts = data.timeSeries.map((entry) => ({
+      time: entry.validTime,
+      parameters: paramArrayToObject(entry.parameters),
+    }));
+
+    // Debuggar processad data
+    console.debug("Parsed response:", forecasts);
+
+    return forecasts;
+  } catch (error) {
+    console.error("Ett fel uppstod:", error);
+    throw error;
+  }
 }
